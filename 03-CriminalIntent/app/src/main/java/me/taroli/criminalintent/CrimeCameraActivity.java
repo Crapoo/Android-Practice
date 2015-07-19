fa22ed1e7fb0567eaf7b780bc0094cbd23139363
Matteo Taroli <taroli.matteo@gmail.com>
2015-07-19 19:01:57 +0200

    Remove actionbar from camera preview

--- 03-CriminalIntent/app/src/main/java/me/taroli/criminalintent/CrimeCameraActivity.java
+++ 03-CriminalIntent/app/src/main/java/me/taroli/criminalintent/CrimeCameraActivity.java
@@ -1,6 +1,10 @@
 package me.taroli.criminalintent;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by Matt on 19/07/15.
@@ -10,5 +14,14 @@ public class CrimeCameraActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new CrimeCameraFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        /* Hide window title */
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /* Hide status bar */
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
     }
 }
